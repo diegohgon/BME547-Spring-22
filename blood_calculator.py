9b1b00a095a4c5e4b9f6a61c7d2327324d5d11f7
diegohgon <diegohgon@gmail.com>
2022-01-12 14:32:40 -0700

    Developed HDL driver to accept input

--- blood_calculator.py
+++ blood_calculator.py
@@ -8,5 +8,25 @@ def interface ():
         if choice == "9":
             keep_running = False
     return
+
+
+def accept_input(test_name):
+    entry = input("Enter the {} test result: ".format(test_name))
+    return int(entry)
+
+def check_HDL(HDL_value):
+    if HDL_value >= 60:
+        answer = "Normal"
+    elif 60 > HDL_value >= 40:
+        answer = "Borderline Low"
+    else:
+        answer = "Low"
+    return answer
+
+def HDL_driver():
+    HDL_value = accept_input("HDL")
+    classification = check_HDL(HDL_value)
     
+
+
 interface()
