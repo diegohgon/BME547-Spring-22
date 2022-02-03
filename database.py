b8a319bf56a4943b92c8989a578da79dc8e9fea0
diegohgon <diegohgon@gmail.com>
2022-02-03 17:20:47 -0500

    corrected database and blood calculator to be up to PEP8 coding style

--- database.py
+++ database.py
@@ -1,13 +1,16 @@
+from blood_calculator import *
 print("This is the database module and python calls it ()".format(__name__))
 
-from blood_calculator import *
-# Using a star is the easiest but the problem is that it is difficult to keep your work organized 
-# Star is considered poor practice 
-# In your career as a coder you are more likely to spend 2-3x as much time reading code as you are writing it
+# Using a star is the easiest but the problem is that it is difficult to keep
+# your work organized
+# Star is considered poor practice
+# In your career as a coder you are more likely to spend 2-3x as much time
+# reading code as you are writing it
 
 
 # from analysis import check_HDL
-# if you have two packages and within them you have to lines called the same. Python will only run the first one
+# if you have two packages and within them you have to lines called the same.
+# Python will only run the first one
 # To help with this it is better to just import the module
 
 HDL_value = 55
@@ -18,4 +21,3 @@ classificication = check_HDL(HDL_value)
 # but an alias like "bc" is better too
 print("55 is ()".format(classificication))
 x = check_LDL(200)
-
