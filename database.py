e6fe4405e69dc993726c2b3b105aeb2a3e07ac22
diegohgon <diegohgon@gmail.com>
2022-02-07 16:17:05 -0500

    added dictionaries for patients

--- database.py
+++ database.py
@@ -1,23 +1,69 @@
-from blood_calculator import *
-print("This is the database module and python calls it ()".format(__name__))
+def add_patient(patient_name, patient_id, age):
+    new_patient = [patient_name, patient_id, age, []]
+    new_patient = {"name": patient_name,
+                   "id": patient_id,
+                   "age": age,
+                   "tests": []}
+    return new_patient
 
-# Using a star is the easiest but the problem is that it is difficult to keep
-# your work organized
-# Star is considered poor practice
-# In your career as a coder you are more likely to spend 2-3x as much time
-# reading code as you are writing it
+
+def main():
+    db = []
+    x = add_patient("Ann Ables", 342, 40)
+    db.append(x)
+    y = add_patient("Bob Boyles", 50, 50)
+    db.append(y)
+    z = add_patient("Chris Columbus", 111, 35)
+    db.append(z)
+    db.append(add_patient("David Dinkins", 22, 72))
+    found_patient = find_patient(db, 111)
+    print(found_patient)
+    output_database(db)
+    add_test_to_patient(db, 111, "HDL", 100)
+    print(db)
+    output_database(db)
+    return db
 
 
-# from analysis import check_HDL
-# if you have two packages and within them you have to lines called the same.
-# Python will only run the first one
-# To help with this it is better to just import the module
+def output_database(db):
+    for patient in db:
+        output_patient(patient)
 
-HDL_value = 55
 
-classificication = check_HDL(HDL_value)
-# otheranswre analysis.check_HDL()
-# how you could call this second one
-# but an alias like "bc" is better too
-print("55 is ()".format(classificication))
-x = check_LDL(200)
+def output_patient(patient):
+    for key in patient:
+        print("{}: {}".format(key, patient[key]))
+        key = "physician"
+
+
+def find_patient(db, id_no):
+    for patient in db:
+        if patient[1] == id_no:
+            return patient
+    return
+
+
+def add_test_to_patient(db, id_no, test_name, test_result):
+    patient = find_patient(db, id_no)
+    test_tuple = (test_name, test_result)
+    patient[3].append(test_tuple)
+
+
+def print_directory(db):
+    rooms = ["Room 13", "Room 12", "Room 99", "Room 3"]
+    for room, patient in zip(rooms, db):
+        print("{} - {}".format(patient[0], room))
+
+        # print("Name: {}  Room: {}".format(patient[0], rooms[i]))
+
+
+def create_id_tag_string(patient):
+    id_string = "{}: {}".format(patient["name"], patient["id"])
+    return id_string
+
+
+if __name__ == "__main__":
+    db = main()
+    print_directory(db)
+    print(create_id_tag_string(find_patient(db, 111)))
+    print(db[2]["tests"][0][0])
